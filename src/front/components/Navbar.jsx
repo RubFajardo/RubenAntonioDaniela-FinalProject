9da45f66e67bcf37d7544676f431c33af23298a2
RubFajardo <rubenfajardo68@gmail.com>
2025-07-21 17:11:09 +0000

    Cambios

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,35 +1,53 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react"
+import { Link, useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const loggeddUser = localStorage.getItem("user");
+    if (loggeddUser) {
+      console.log(loggeddUser)
+      setUser(JSON.parse(loggeddUser));
+    }
+  }, [user]);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light px-4">
-		<div className="container">
-      <Link className="navbar-brand" to="/">HOME</Link>
+      <div className="container">
+        <Link className="navbar-brand" to="/">HOME</Link>
 
-      <div className="collapse navbar-collapse" id="navbarContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <Link className="nav-link" to="/contacto">Contacto</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/vision">Visión</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/habitos">Hábitos</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/agenda">Agenda</Link>
-          </li>
-        </ul>
+        <div className="collapse navbar-collapse" id="navbarContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" to="/contacto">Contacto</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/vision">Visión</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/habitos">Hábitos</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/agenda">Agenda</Link>
+            </li>
+          </ul>
 
-        <div className="d-flex">
-          <Link className="btn btn-outline-primary" to="/login">
-            Iniciar sesión
-          </Link>
+          <div className="d-flex">
+            {user ? (
+              <span className="navbar-text">Bienvenido, {user.name} <button className="btn btn-outline-danger ms-3" onClick={() => {
+                localStorage.removeItem("token");
+                localStorage.removeItem("user");
+                navigate("/login");
+
+              }}>Cerrar sesión</button></span>
+            ) : (
+              <Link className="btn btn-outline-primary" to="/login">Iniciar sesión</Link>
+            )}
+          </div>
         </div>
       </div>
-	  </div>
     </nav>
   );
 };
