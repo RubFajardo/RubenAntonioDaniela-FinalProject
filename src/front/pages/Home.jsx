dbac27693877f40e206e2674f41755fb1ee24964
danipadi13 <159074064+danipadi13@users.noreply.github.com>
2025-07-23 16:58:52 +0000

    cambio

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -14,13 +14,13 @@ export const Home = () => {
         <div id="carouselHabitos" className="carousel slide w-100" data-bs-ride="carousel">
           <div className="carousel-inner">
             <div className="carousel-item active">
-              <img src="https://via.placeholder.com/1200x500?text=Ejercicio+Físico" className="d-block w-100" alt="Ejercicio" />
+              <img src="https://www.google.com/imgres?q=ejercicios%20persona%20levantando%20pesas%20habito&imgurl=https%3A%2F%2Fwww.myprotein.es%2Fimages%3Furl%3Dhttps%3A%2F%2Fblogscdn.thehut.net%2Fapp%2Fuploads%2Fsites%2F450%2F2020%2F07%2FiStock-1175815322opt_hero_1593679552_1593778674_1200x672_acf_cropped.jpg%26auto%3Davif%26width%3D1200%26fit%3Dcrop&imgrefurl=https%3A%2F%2Fwww.myprotein.es%2Fthezone%2Fentrenamiento%2Flevantamiento-de-pesas-para-principiantes%2F&docid=TzH41h1bp5oMJM&tbnid=XsuugTmWDo3GUM&vet=12ahUKEwiHifPTudOOAxWBAPsDHbsbGv4QM3oECBEQAA..i&w=1200&h=672&hcb=2&ved=2ahUKEwiHifPTudOOAxWBAPsDHbsbGv4QM3oECBEQAA" className="d-block w-100" alt="Ejercicio" />
             </div>
             <div className="carousel-item">
-              <img src="https://via.placeholder.com/1200x500?text=Alimentación+Saludable" className="d-block w-100" alt="Alimentación" />
+              <img src="https://www.google.com/imgres?q=ejercicios%20persona%20levantando%20pesas%20habito&imgurl=https%3A%2F%2Fwww.myprotein.es%2Fimages%3Furl%3Dhttps%3A%2F%2Fblogscdn.thehut.net%2Fapp%2Fuploads%2Fsites%2F450%2F2020%2F07%2FiStock-1175815322opt_hero_1593679552_1593778674_1200x672_acf_cropped.jpg%26auto%3Davif%26width%3D1200%26fit%3Dcrop&imgrefurl=https%3A%2F%2Fwww.myprotein.es%2Fthezone%2Fentrenamiento%2Flevantamiento-de-pesas-para-principiantes%2F&docid=TzH41h1bp5oMJM&tbnid=XsuugTmWDo3GUM&vet=12ahUKEwiHifPTudOOAxWBAPsDHbsbGv4QM3oECBEQAA..i&w=1200&h=672&hcb=2&ved=2ahUKEwiHifPTudOOAxWBAPsDHbsbGv4QM3oECBEQAA" className="d-block w-100" alt="Alimentación" />
             </div>
             <div className="carousel-item">
-              <img src="https://via.placeholder.com/1200x500?text=Sueño+Reparador" className="d-block w-100" alt="Sueño" />
+              <img src="https://www.google.com/imgres?q=ejercicios%20persona%20levantando%20pesas%20habito&imgurl=https%3A%2F%2Fwww.myprotein.es%2Fimages%3Furl%3Dhttps%3A%2F%2Fblogscdn.thehut.net%2Fapp%2Fuploads%2Fsites%2F450%2F2020%2F07%2FiStock-1175815322opt_hero_1593679552_1593778674_1200x672_acf_cropped.jpg%26auto%3Davif%26width%3D1200%26fit%3Dcrop&imgrefurl=https%3A%2F%2Fwww.myprotein.es%2Fthezone%2Fentrenamiento%2Flevantamiento-de-pesas-para-principiantes%2F&docid=TzH41h1bp5oMJM&tbnid=XsuugTmWDo3GUM&vet=12ahUKEwiHifPTudOOAxWBAPsDHbsbGv4QM3oECBEQAA..i&w=1200&h=672&hcb=2&ved=2ahUKEwiHifPTudOOAxWBAPsDHbsbGv4QM3oECBEQAA" className="d-block w-100" alt="Sueño" />
             </div>
           </div>
           <button className="carousel-control-prev" type="button" data-bs-target="#carouselHabitos" data-bs-slide="prev">
@@ -41,7 +41,7 @@ export const Home = () => {
             </div>
             <div className="carousel-item">
               <div className="testimonial">"El seguimiento me ayudó a ser constante y ver resultados. Es como tener un coach personal." – Miguel A.</div>
-            </div>
+            </div
             <div className="carousel-item">
               <div className="testimonial">"Ahora duermo mejor, como mejor y hago ejercicio sin sentirlo como una obligación." – Paula G.</div>
             </div>
