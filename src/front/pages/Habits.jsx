82188aae19b00d316467c4e8379f90ddacf4148e
Antonio Borrero <antojoseabad@gmail.com>
2025-07-30 17:54:55 +0000

    aplicacion de ruta de back global para login, habits y register y validaciones para post de habits

--- src/front/pages/Habits.jsx
+++ src/front/pages/Habits.jsx
@@ -6,6 +6,7 @@ export const Habits = () => {
 
   const [userName, setUserName] = useState("");
   const navigate = useNavigate();
+  const backendUrl = import.meta.env.VITE_BACKEND_URL
 
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
@@ -82,20 +83,33 @@ export const Habits = () => {
       alert("No has iniciado sesion");
       return;
     }
-    console.log({
-        date: today,
-        habits: [
-          {
-            entreno: didTrain,
-            ejercicio: trainingType,
-            sueño: sleepQuality,
-            calorias: foodTotal.caloriesTotal,
-            proteinas: foodTotal.proteinTotal,
-          }
-        ]
-      })
-      console.log(token)
-    await fetch("https://ominous-parakeet-jj76wwq7q4x735vjj-3001.app.github.dev/api/daily_habits", {
+
+    if (didTrain === null) {
+      return alert("Por favor, indica si entrenaste hoy.");
+    }
+    if (didTrain === true && trainingType === '') {
+      return alert("Por favor, selecciona el tipo de entrenamiento.");
+    }
+    if (didtrain === false && trainingType !== '') {
+      return alert("Si no entrenaste, no es necesario seleccionar el tipo de entrenamiento.");
+    }
+    if (sleepQuality === '') {
+      return alert("Por favor, indica la calidad de tu sueño.");
+    }
+    if (breakfast.calories <= 0 && lunch.calories <= 0 && dinner.calories <= 0) {
+      return alert("Por favor, ingresa al menos una comida con calorías.");
+    }
+    if (breakfast.calories > 5000 || lunch.calories > 5000 || dinner.calories > 5000) {
+      return alert("Por favor, verifica las caalorias ingresadas, es un valor muy alto.");
+    }
+    if (breakfast.protein <= 0 && lunch.protein <= 0 && dinner.protein <= 0) {
+      return alert("Por favor, ingresa al menos una comida con proteínas.");
+    }
+    if (breakfast.protein > 500 || lunch.protein > 500 || dinner.protein > 500) {
+      return alert("Por favor, verifica las proteínas ingresadas, es un valor muy alto.");
+    }
+
+    await fetch(backendUrl + "api/daily_habits", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -115,12 +129,12 @@ export const Habits = () => {
       }),
     },
     );
-   
+
   }
 
   return (
     <div className="container mt-5">
-      <h2 className="text-center mb-4">¡Tus hábitos saludables de hoy!</h2>git
+      <h2 className="text-center mb-4">¡Tus hábitos saludables de hoy!</h2>
 
       <form onSubmit={handleSubmit}>
         <div className="card p-4 mb-4 shadow-sm">
@@ -139,7 +153,7 @@ export const Habits = () => {
           <h6 className="mb-3">¿Qué entrenaste hoy?</h6>
           {["Tren Superior", "Tren Inferior", "Cardio"].map((part, index) => (
             <div className="form-check" key={index}>
-              <input className="form-check-input" type="radio" onChange={() => setTrainingType(`${part}`)} name="trainingType" id={`type${index}`} value={part} checked={trainingType === part} />
+              <input className="form-check-input" type="radio" onChange={() => setTrainingType(`${part}`)} name="trainingType" id={`type${index}`} value={part} checked={trainingType === part} disabled={didTrain === false}/>
               <label className="form-check-label" htmlFor={`type${index}`}>{part}</label>
             </div>
           ))}
